d8c0fd62a2f45e61c5197fccad04c10126f43da5
Pritesh2809 <rspritesh009@gmail.com>
2025-01-07 23:04:28 +0530

    Delete Rs100Note.java

--- Rs100Note.java
+++
@@ -1,9 +0,0 @@
-// Rs100Note class to represent a 100 Rs note
-public class Rs100Note extends NotesAction.Note { // Declaring Rs100Note class that extends Note
-
-    // Constructor for Rs100Note
-    public Rs100Note() {
-
-        super(100); // Calling constructor of superclass Note with value 100
-    }
-}
